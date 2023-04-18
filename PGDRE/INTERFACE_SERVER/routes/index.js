58fa55db7fcd635a2ff2b58a23d16a5cb7fb8bd2
Gonçalo <smgonsa@hotmail.com>
2023-04-18 18:30:49 +0100

    xd

--- PGDRE/INTERFACE_SERVER/routes/index.js
+++ PGDRE/INTERFACE_SERVER/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+/*                                GETS                                   */
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = new Date().toISOString().substring(0,16)
@@ -15,6 +16,16 @@ router.get('/register', function(req, res){
 router.get('/login', function(req, res){
   var data = new Date().toISOString().substring(0,16) 
   res.render('loginForm', {d: data})
+})
+
+/*                                POSTS                                 */
+
+router.post('/register', function(req,res){
+
+})
+
+router.post('/login', function(req, res){
+
 })
 
 module.exports = router;
--- PGDRE/INTERFACE_SERVER/routes/users.js
+++ PGDRE/INTERFACE_SERVER/routes/users.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Provavelmente este ficheiro vai ser para ELIMINAR */
+
 module.exports = router;
