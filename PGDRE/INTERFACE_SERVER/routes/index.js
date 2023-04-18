edb4470e7e76710f20272c7226449a7ac80aae13
Gonçalo <smgonsa@hotmail.com>
2023-04-18 17:20:33 +0100

    xD

--- PGDRE/INTERFACE_SERVER/routes/index.js
+++ PGDRE/INTERFACE_SERVER/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = new Date().toISOString().substring(0,16)
+  res.render('index', {d: data});
 });
+
+router.get('/register', function(req, res){
+  var data = new Date().toISOString().substring(0,16) 
+  res.render('registerForm', {d: data})
+})
+
+router.get('/login', function(req, res){
+  var data = new Date().toISOString().substring(0,16) 
+  res.render('loginForm', {d: data})
+})
 
 module.exports = router;
